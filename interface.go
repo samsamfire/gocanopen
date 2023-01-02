187e48bea5d2ca7108be12d66f542d5f04b98647
Samuel Lee <samfire.pilote@gmail.com>
2023-01-02 19:58:16 +0100

    Added most of the interface implementation, some things might still change

--- interface.go
+++ interface.go
@@ -15,130 +15,220 @@ const (
 	/** Read/write is only partial, make more calls */
 	ODR_PARTIAL ODR = -1
 	/** SDO abort 0x00000000 - Read/write successfully finished */
-	ODR_OK = 0
+	ODR_OK ODR = 0
 	/** SDO abort 0x05040005 - Out of memory */
-	ODR_OUT_OF_MEM = 1
+	ODR_OUT_OF_MEM ODR = 1
 	/** SDO abort 0x06010000 - Unsupported access to an object */
-	ODR_UNSUPP_ACCESS = 2
+	ODR_UNSUPP_ACCESS ODR = 2
 	/** SDO abort 0x06010001 - Attempt to read a write only object */
-	ODR_WRITEONLY = 3
+	ODR_WRITEONLY ODR = 3
 	/** SDO abort 0x06010002 - Attempt to write a read only object */
-	ODR_READONLY = 4
+	ODR_READONLY ODR = 4
 	/** SDO abort 0x06020000 - Object does not exist in the object dict. */
 	ODR_IDX_NOT_EXIST = 5
 	/** SDO abort 0x06040041 - Object cannot be mapped to the PDO */
-	ODR_NO_MAP = 6
+	ODR_NO_MAP ODR = 6
 	/** SDO abort 0x06040042 - PDO length exceeded */
-	ODR_MAP_LEN = 7
+	ODR_MAP_LEN ODR = 7
 	/** SDO abort 0x06040043 - General parameter incompatibility reasons */
-	ODR_PAR_INCOMPAT = 8
+	ODR_PAR_INCOMPAT ODR = 8
 	/** SDO abort 0x06040047 - General internal incompatibility in device */
-	ODR_DEV_INCOMPAT = 9
+	ODR_DEV_INCOMPAT ODR = 9
 	/** SDO abort 0x06060000 - Access failed due to hardware error */
-	ODR_HW = 10
+	ODR_HW ODR = 10
 	/** SDO abort 0x06070010 - Data type does not match */
-	ODR_TYPE_MISMATCH = 11
+	ODR_TYPE_MISMATCH ODR = 11
 	/** SDO abort 0x06070012 - Data type does not match, length too high */
-	ODR_DATA_LONG = 12
+	ODR_DATA_LONG ODR = 12
 	/** SDO abort 0x06070013 - Data type does not match, length too short */
-	ODR_DATA_SHORT = 13
+	ODR_DATA_SHORT ODR = 13
 	/** SDO abort 0x06090011 - Sub index does not exist */
-	ODR_SUB_NOT_EXIST = 14
+	ODR_SUB_NOT_EXIST ODR = 14
 	/** SDO abort 0x06090030 - Invalid value for parameter (download only) */
-	ODR_INVALID_VALUE = 15
+	ODR_INVALID_VALUE ODR = 15
 	/** SDO abort 0x06090031 - Value range of parameter written too high */
-	ODR_VALUE_HIGH = 16
+	ODR_VALUE_HIGH ODR = 16
 	/** SDO abort 0x06090032 - Value range of parameter written too low */
-	ODR_VALUE_LOW = 17
+	ODR_VALUE_LOW ODR = 17
 	/** SDO abort 0x06090036 - Maximum value is less than minimum value */
-	ODR_MAX_LESS_MIN = 18
+	ODR_MAX_LESS_MIN ODR = 18
 	/** SDO abort 0x060A0023 - Resource not available: SDO connection */
-	ODR_NO_RESOURCE = 19
+	ODR_NO_RESOURCE ODR = 19
 	/** SDO abort 0x08000000 - General error */
-	ODR_GENERAL = 20
+	ODR_GENERAL ODR = 20
 	/** SDO abort 0x08000020 - Data cannot be transferred or stored to app */
-	ODR_DATA_TRANSF = 21
+	ODR_DATA_TRANSF ODR = 21
 	/** SDO abort 0x08000021 - Data can't be transferred (local control) */
-	ODR_DATA_LOC_CTRL = 22
+	ODR_DATA_LOC_CTRL ODR = 22
 	/** SDO abort 0x08000022 - Data can't be transf. (present device state) */
-	ODR_DATA_DEV_STATE = 23
+	ODR_DATA_DEV_STATE ODR = 23
 	/** SDO abort 0x08000023 - Object dictionary not present */
-	ODR_OD_MISSING = 24
+	ODR_OD_MISSING ODR = 24
 	/** SDO abort 0x08000024 - No data available */
-	ODR_NO_DATA = 25
+	ODR_NO_DATA ODR = 25
 	/** Last element, number of responses */
-	ODR_COUNT = 26
+	ODR_COUNT ODR = 26
 )
 
-type OD_stream_t struct {
+var SDO_ABORT_MAP = map[ODR]uint32{
+	0:  0x00000000, /* No abort */
+	1:  0x05040005, /* Out of memory */
+	2:  0x06010000, /* Unsupported access to an object */
+	3:  0x06010001, /* Attempt to read a write only object */
+	4:  0x06010002, /* Attempt to write a read only object */
+	5:  0x06020000, /* Object does not exist in the object dictionary */
+	6:  0x06040041, /* Object cannot be mapped to the PDO */
+	7:  0x06040042, /* Num and len of object to be mapped exceeds PDO len */
+	8:  0x06040043, /* General parameter incompatibility reasons */
+	9:  0x06040047, /* General internal incompatibility in device */
+	10: 0x06060000, /* Access failed due to hardware error */
+	11: 0x06070010, /* Data type does not match, length does not match */
+	12: 0x06070012, /* Data type does not match, length too high */
+	13: 0x06070013, /* Data type does not match, length too short */
+	14: 0x06090011, /* Sub index does not exist */
+	15: 0x06090030, /* Invalid value for parameter (download only). */
+	16: 0x06090031, /* Value range of parameter written too high */
+	17: 0x06090032, /* Value range of parameter written too low */
+	18: 0x06090036, /* Maximum value is less than minimum value. */
+	19: 0x060A0023, /* Resource not available: SDO connection */
+	20: 0x08000000, /* General error */
+	21: 0x08000020, /* Data cannot be transferred or stored to application */
+	22: 0x08000021, /* Data cannot be transferred because of local control */
+	23: 0x08000022, /* Data cannot be tran. because of present device state */
+	24: 0x08000023, /* Object dict. not present or dynamic generation fails */
+	25: 0x08000024, /* No data available */
+}
+
+//Get the associated abort code, if the code is not present in map, return ODR_DEV_INCOMPAT
+func (result ODR) GetSDOAbordCode() uint32 {
+	abort_code, ok := SDO_ABORT_MAP[result]
+	if ok {
+		return abort_code
+	} else {
+		return SDO_ABORT_MAP[ODR_DEV_INCOMPAT]
+	}
+}
+
+const (
+	/** This type corresponds to CANopen Object Dictionary object with object
+	 * code equal to VAR. OD object is type of @ref OD_obj_var_t and represents
+	 * single variable of any type (any length), located on sub-index 0. Other
+	 * sub-indexes are not used. */
+	ODT_VAR = 0x01
+	/** This type corresponds to CANopen Object Dictionary object with object
+	 * code equal to ARRAY. OD object is type of @ref OD_obj_array_t and
+	 * represents array of variables with the same type, located on sub-indexes
+	 * above 0. Sub-index 0 is of type uint8_t and usually represents length of
+	 * the array. */
+	ODT_ARR = 0x02
+	/** This type corresponds to CANopen Object Dictionary object with object
+	 * code equal to RECORD. This type of OD object represents structure of
+	 * the variables. Each variable from the structure can have own type and
+	 * own attribute. OD object is an array of elements of type
+	 * @ref OD_obj_var_t. Variable at sub-index 0 is of type uint8_t and usually
+	 * represents number of sub-elements in the structure. */
+	ODT_REC = 0x03
+	/** Mask for basic type */
+	ODT_TYPE_MASK = 0x0F
+)
+
+/**
+ * Attributes (bit masks) for OD sub-object.
+ */
+type ODA uint8
+
+const (
+	ODA_SDO_R  ODA = 0x01 /**< SDO server may read from the variable */
+	ODA_SDO_W  ODA = 0x02 /**< SDO server may write to the variable */
+	ODA_SDO_RW ODA = 0x03 /**< SDO server may read from or write to the variable */
+	ODA_TPDO   ODA = 0x04 /**< Variable is mappable into TPDO (can be read) */
+	ODA_RPDO   ODA = 0x08 /**< Variable is mappable into RPDO (can be written) */
+	ODA_TRPDO  ODA = 0x0C /**< Variable is mappable into TPDO or RPDO */
+	ODA_TSRDO  ODA = 0x10 /**< Variable is mappable into transmitting SRDO */
+	ODA_RSRDO  ODA = 0x20 /**< Variable is mappable into receiving SRDO */
+	ODA_TRSRDO ODA = 0x30 /**< Variable is mappable into tx or rx SRDO */
+	ODA_MB     ODA = 0x40 /**< Variable is multi-byte ((u)int16_t to (u)int64_t) */
+	ODA_STR    ODA = 0x80 /**< Shorter value, than specified variable size, may be
+	  written to the variable. SDO write will fill remaining memory with zeroes.
+	  Attribute is used for VISIBLE_STRING and UNICODE_STRING. */
+)
+
+type ODStream struct {
 	/** Pointer to original data object, defined by Object Dictionary. Default
 	 * read/write functions operate on it. If memory for data object is not
 	 * specified by Object Dictionary, then dataOrig is NULL.
 	 */
-	dataOrig []uint8
+	dataOrig []byte
 	/** Pointer to object, passed by @ref OD_extension_init(). Can be used
 	 * inside read / write functions from IO extension.
 	 */
-	object *uint8
+	object interface{}
 	/** In case of large data, dataOffset indicates position of already
 	 * transferred data */
 	dataOffset uint32
 	/** Attribute bit-field of the OD sub-object, see @ref OD_attributes_t */
-	attribute uint8
+	attribute ODA
 	/** Sub index of the OD sub-object, informative */
 	subIndex uint8
+}
+
+func NewODStream() ODStream {
+	return ODStream{dataOrig: nil, object: nil, dataOffset: 0, attribute: 0, subIndex: 0}
 }
 
 /**
  * Extension of OD object, which can optionally be specified by application in
  * initialization phase with @ref OD_extension_init() function.
  */
-type OD_Extension_t struct {
-	/** Object on which read and write will operate, part of @ref OD_stream_t */
-	object *interface{}
-	/** Application specified read function pointer. If NULL, then read will be
-	 * disabled. @ref OD_readOriginal can be used here to keep the original read
-	 * function. For function description see @ref OD_IO_t. */
-	read func(stream *OD_stream_t, buf []uint8, countRead *uint32) uint8
-	/** Application specified write function pointer. If NULL, then write will
-	 * be disabled. @ref OD_writeOriginal can be used here to keep the original
-	 * write function. For function description see @ref OD_IO_t. */
-	write func(stream *OD_stream_t, buf []uint8, countWritten *uint32) uint8
-	/**PDO flags bit-field provides one bit for each OD variable, which exist
-	 * inside OD object at specific sub index. If application clears that bit,
-	 * and OD variable is mapped to an event driven TPDO, then TPDO will be
-	 * sent.
-	 *
-	 * @ref OD_FLAGS_PDO_SIZE can have a value from 0 to 32 bytes, which
-	 * corresponds to 0 to 256 available bits. If, for example,
-	 * @ref OD_FLAGS_PDO_SIZE has value 4, then OD variables with sub index up
-	 * to 31 will have the TPDO requesting functionality.
-	 * See also @ref OD_requestTPDO and @ref OD_TPDOtransmitted. */
+type Extension struct {
+	/** Object on which read and write will operate, part of @ref ODStream */
+	object   *interface{}
+	Read     func(buffer []byte) (result ODR, n uint32)
+	Write    func(buffer []byte) (result ODR, n uint32)
 	flagsPDO [4]uint8
 }
 
-type OD_IO_t struct {
+type OD_io struct {
+	/** Object Dictionary stream object, passed to read or write */
+	stream ODStream
+	Read   func(buffer []byte) (result ODR, n uint32)
+	Write  func(buffer []byte) (result ODR, n uint32)
 }
 
-type OD_entry_t struct {
-	/** Object Dictionary index */
-	index uint16
-	/** Number of all sub-entries, including sub-entry at sub-index 0 */
-	subEntriesCount uint8
-	/** Type of the odObject, indicated by @ref OD_objectTypes_t enumerator. */
-	odObjectType uint8
-	/** OD object of type indicated by odObjectType, from which @ref OD_getSub()
-	* fetches the information */
-	odObject *interface{}
-	/** Extension to OD, specified by application */
-	extension *OD_Extension_t
+type Entry struct {
+	index     uint16
+	odObject  interface{}
+	extension *Extension
 }
 
-type OD_t struct {
-	/** Number of elements in the list, without last element, which is blank */
-	size uint16
-	/** List OD entries (table of contents), ordered by index */
-	list []OD_entry_t
+type ObjectDictionary struct {
+	list []Entry
+}
+
+/*basic OD variable */
+type Variable struct {
+	Data      []byte
+	Attribute ODA
+}
+
+/**
+ * Object for OD array of variables, used for "ARRAY" type OD objects
+ */
+type Array struct {
+	Data0             []byte /**< Pointer to data for sub-index 0 */
+	Data              []byte /**< Pointer to array of data */
+	Attribute0        ODA
+	Attribute         ODA /**< Attribute bitfield for array elements */
+	DataElementLength uint32
+}
+
+/**
+ * Object for OD sub-elements, used in "RECORD" type OD objects
+ */
+type Record struct {
+	Data      []byte
+	Subindex  uint8
+	Attribute ODA
 }
 
 /**
@@ -151,11 +241,12 @@ type OD_t struct {
  * also @ref OD_IO_t.
  */
 
-func (stream OD_stream_t) ReadOriginal(count uint32) (result ODR, data []uint8) {
+func (stream *ODStream) ReadOriginal(data []byte) (result ODR, n uint32) {
 	dataLenToCopy := uint32(len(stream.dataOrig)) /* length of OD variable */
+	count := uint32(len(data))
 	dataOrig := stream.dataOrig
 	if dataOrig == nil {
-		return ODR_SUB_NOT_EXIST, nil
+		return ODR_SUB_NOT_EXIST, 0
 	}
 
 	var returnCode ODR
@@ -166,7 +257,7 @@ func (stream OD_stream_t) ReadOriginal(count uint32) (result ODR, data []uint8) 
 
 	if stream.dataOffset > 0 || dataLenToCopy > count {
 		if stream.dataOffset >= dataLenToCopy {
-			return ODR_DEV_INCOMPAT, nil
+			return ODR_DEV_INCOMPAT, 0
 		}
 		/* Reduce for already copied data */
 		dataLenToCopy -= stream.dataOffset
@@ -180,19 +271,20 @@ func (stream OD_stream_t) ReadOriginal(count uint32) (result ODR, data []uint8) 
 			stream.dataOffset = 0 /* copy finished, reset offset */
 		}
 	}
-	return returnCode, stream.dataOrig[stream.dataOffset : stream.dataOffset+dataLenToCopy]
+	copy(data, stream.dataOrig[stream.dataOffset:stream.dataOffset+dataLenToCopy])
+	return returnCode, dataLenToCopy
 }
 
-func (stream OD_stream_t) WriteOriginal(data []uint8) (result ODR) {
+func (stream ODStream) WriteOriginal(data []byte) (result ODR, n uint32) {
 	if data == nil {
-		return ODR_DEV_INCOMPAT
+		return ODR_DEV_INCOMPAT, 0
 	}
 	dataLenToCopy := uint32(len(stream.dataOrig)) /* length of OD variable */
 	dataOrig := stream.dataOrig
 	count := uint32(len(data))
 
 	if dataOrig == nil {
-		return ODR_SUB_NOT_EXIST
+		return ODR_SUB_NOT_EXIST, 0
 	}
 
 	var returnCode ODR
@@ -204,7 +296,7 @@ func (stream OD_stream_t) WriteOriginal(data []uint8) (result ODR) {
 
 	if stream.dataOffset > 0 || dataLenToCopy > count {
 		if stream.dataOffset >= dataLenToCopy {
-			return ODR_DEV_INCOMPAT
+			return ODR_DEV_INCOMPAT, 0
 		}
 		/* reduce for already copied data */
 		dataLenToCopy -= stream.dataOffset
@@ -222,8 +314,178 @@ func (stream OD_stream_t) WriteOriginal(data []uint8) (result ODR) {
 
 	if dataLenToCopy < count {
 		/* OD variable is smaller than current amount of data */
-		return ODR_DATA_LONG
+		return ODR_DATA_LONG, 0
 	}
 	copy(stream.dataOrig[stream.dataOffset:stream.dataOffset+dataLenToCopy], data)
-	return returnCode
+	return returnCode, dataLenToCopy
+}
+
+/* Write value to variable from Object Dictionary disabled, see OD_IO_t */
+func (stream ODStream) WriteDisabled(data []byte) (result ODR, n uint32) {
+	return ODR_UNSUPP_ACCESS, 0
+}
+
+/* Read value from variable from Object Dictionary disabled, see OD_IO_t*/
+func (stream ODStream) ReadDisabled(data []byte) (result ODR, n uint32) {
+	return ODR_UNSUPP_ACCESS, 0
+}
+
+/**
+ * Find OD entry in Object Dictionary
+ *
+ * @param od Object Dictionary
+ * @param index CANopen Object Dictionary index of object in Object Dictionary
+ *
+ * @return Pointer to OD entry or NULL if not found
+ */
+
+func (od *ObjectDictionary) Find(index uint16) (entry *Entry) {
+	if od == nil || len(od.list) == 0 {
+		return nil
+	}
+	min := 0
+	max := len(od.list) - 1
+
+	/* Fast search (binary search) in ordered Object Dictionary. If indexes are mixed,
+	 * this won't work. If Object Dictionary has up to N entries, then the
+	 * max number of loop passes is log2(N) */
+	for min < max {
+		/* get entry between min and max */
+		cur := (min + max) >> 1
+		entry = &od.list[cur]
+
+		if index == entry.index {
+			return entry
+		}
+
+		if index < entry.index {
+			if cur > 0 {
+				max = cur - 1
+			} else {
+				max = cur
+			}
+		} else {
+			min = cur + 1
+		}
+	}
+
+	if min == max {
+		entry = &od.list[min]
+		if index == entry.index {
+			return entry
+		}
+	}
+
+	return nil /* entry does not exist in OD */
+}
+
+/**
+ * Find sub-object with specified sub-index on OD entry returned by OD_find.
+ * Function populates io structure with sub-object data.
+ *
+ * @warning
+ * Read and write functions may be called from different threads, so critical
+ * sections in custom functions must be observed, see @ref CO_critical_sections.
+ *
+ * @param entry OD entry returned by @ref OD_find().
+ * @param subIndex Sub-index of the variable from the OD object.
+ * @param [out] io Structure will be populated on success.
+ * @param odOrig If true, then potential IO extension on entry will be
+ * ignored and access to data entry in the original OD location will be returned
+ *
+ * @return Value from @ref ODR_t, "ODR_OK" in case of success.
+ */
+
+func (entry *Entry) Sub(subindex uint8, origin bool) (result ODR, io *OD_io) {
+
+	if entry == nil || entry.odObject == nil {
+		return ODR_IDX_NOT_EXIST, nil
+	}
+
+	io = &OD_io{stream: NewODStream(), Read: nil, Write: nil}
+	stream := io.stream
+	object := entry.odObject
+	/* attribute, dataOrig and dataLength, depends on object type */
+	switch object := object.(type) {
+	case Variable:
+		if subindex > 0 {
+			return ODR_SUB_NOT_EXIST, nil
+		}
+		stream.attribute = object.Attribute
+		stream.dataOrig = object.Data
+
+	case Array:
+		subEntriesCount := 1 + len(object.Data)/int(object.DataElementLength)
+		if subindex >= uint8(subEntriesCount) {
+			return ODR_SUB_NOT_EXIST, nil
+		}
+		if subindex == 0 {
+			stream.attribute = object.Attribute0
+			stream.dataOrig = object.Data0
+			// datalength is 0
+		} else {
+			stream.attribute = object.Attribute
+			// Get the according bytes
+			stream.dataOrig = object.Data[object.DataElementLength*uint32(subindex-1) : object.DataElementLength*uint32(subindex)]
+		}
+
+	case []Record:
+		records := object
+		var record *Record
+		for i := range records {
+			if records[i].Subindex == subindex {
+				record = &records[i]
+			}
+		}
+		if record == nil {
+			return ODR_SUB_NOT_EXIST, nil
+		}
+		stream.attribute = record.Attribute
+		stream.dataOrig = record.Data
+
+	default:
+		return ODR_DEV_INCOMPAT, nil
+	}
+
+	/*Populate read/write function pointers either with default or extension provided*/
+	if entry.extension == nil || origin {
+		io.Read = stream.ReadOriginal
+		io.Write = stream.WriteOriginal
+		stream.object = nil
+	} else {
+		if entry.extension.Read == nil {
+			io.Read = stream.ReadDisabled
+		} else {
+			io.Read = entry.extension.Read
+		}
+		if entry.extension.Write == nil {
+			io.Write = stream.WriteDisabled
+		} else {
+			io.Write = entry.extension.Write
+		}
+		stream.object = entry.extension.object
+	}
+
+	/* Reset stream data offset */
+	stream.dataOffset = 0
+	stream.subIndex = subindex
+
+	return ODR_OK, io
+}
+
+/* Create a new Object dictionary Entry of Variable type */
+func NewVariableEntry(index uint16, data []byte, attribute ODA) Entry {
+	odObject := Variable{Data: data, Attribute: attribute}
+	return Entry{index: index, odObject: odObject, extension: nil}
+}
+
+/* Create a new Object dictionary Entry of Record type, odObject is an empty slice of Record elements */
+func NewRecordEntry(index uint16, records []Record) Entry {
+	return Entry{index: index, odObject: records, extension: nil}
+}
+
+/* Create a new Object dictionary Entry of Array type*/
+func NewArrayEntry(index uint16, data0 []byte, data []byte, attribute0 ODA, attribute ODA, element_length_bytes uint32) Entry {
+	odObject := Array{Data0: data0, Data: data, Attribute0: attribute0, Attribute: attribute, DataElementLength: element_length_bytes}
+	return Entry{index: index, odObject: odObject, extension: nil}
 }
---
+++ interface_test.go
@@ -0,0 +1,72 @@
+package canopen
+
+import "testing"
+
+func createOD() ObjectDictionary {
+	od := ObjectDictionary{
+		[]Entry{
+			NewVariableEntry(0x1016, []byte{0x10, 0x20, 0x10, 0x20}, ODA_SDO_R|ODA_SDO_W),
+			NewVariableEntry(0x1017, []byte{0x10, 0x20, 0x10, 0x20}, ODA_SDO_R|ODA_SDO_W),
+			NewVariableEntry(0x1018, []byte{0x10, 0x20, 0x10, 0x20}, ODA_SDO_R|ODA_SDO_W),
+			NewVariableEntry(0x1019, []byte{0x10, 0x20, 0x10, 0x20}, ODA_SDO_R|ODA_SDO_W),
+			NewVariableEntry(0x1020, []byte{0x10, 0x20, 0x10, 0x20}, ODA_SDO_R|ODA_SDO_W),
+			NewRecordEntry(0x1030, []Record{
+				Record{Data: []byte{0x10, 0x20}, Subindex: 0, Attribute: ODA_SDO_R | ODA_SDO_W},
+				Record{Data: []byte{0x10, 0x20}, Subindex: 1, Attribute: ODA_SDO_R | ODA_SDO_W},
+				Record{Data: []byte{0x10, 0x20}, Subindex: 2, Attribute: ODA_SDO_R | ODA_SDO_W},
+			}),
+		},
+	}
+
+	return od
+}
+
+func TestFind(t *testing.T) {
+	entry1 := NewVariableEntry(0x1016, []byte{0x10, 0x20}, ODA_SDO_R|ODA_SDO_W)
+	entry2 := NewVariableEntry(0x1017, []byte{0x10, 0x20}, ODA_SDO_R|ODA_SDO_W)
+	entries := make([]Entry, 2)
+	entries[0] = entry1
+	entries[1] = entry2
+	od := ObjectDictionary{entries}
+	entry := od.Find(0x1018)
+	if entry != nil {
+		t.Errorf("Entry should be nil")
+
+	}
+
+	entry = od.Find(0x1016)
+	if entry.index != 0x1016 {
+		t.Errorf("Wrong index %x", entry.index)
+	}
+
+}
+
+func TestSub(t *testing.T) {
+	od := createOD()
+	entry := od.Find(0x1020)
+	// Test access to subindex > 1 for variable
+	result, _ := entry.Sub(1, true)
+	if result != ODR_SUB_NOT_EXIST {
+		t.Error()
+	}
+	// Test that subindex 0 returns ODR_OK
+	result, _ = entry.Sub(0, true)
+	if result != ODR_OK {
+		t.Error()
+	}
+	// Test access to subindex 0 of Record should return ODR_OK
+	entry = od.Find(0x1030)
+	result, _ = entry.Sub(0, true)
+	if result != ODR_OK {
+		t.Error()
+	}
+	// Test access to out of range subindex
+	result, _ = entry.Sub(10, true)
+	if result != ODR_SUB_NOT_EXIST {
+		t.Error()
+	}
+
+	// if result != 0 {
+	// 	t.Errorf("ODR is not 0 %d", result)
+	// }
+}
