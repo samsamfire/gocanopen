4fc1deffe251f819c33367856d25d35d2fa6f420
Samuel Lee <samfire.pilote@gmail.com>
2023-02-22 20:18:44 +0100

    - added time.go

---
+++ time.go
@@ -0,0 +1,141 @@
+package canopen
+
+import (
+	"encoding/binary"
+	"time"
+
+	"github.com/brutella/can"
+	log "github.com/sirupsen/logrus"
+)
+
+type TIME struct {
+	RawTimestamp       [6]byte
+	Ms                 uint32 // Milliseconds after midnight
+	Days               uint16 // Days since 1st january 1984
+	ResidualUs         uint16 // Residual Us calculated when processed
+	IsConsumer         bool
+	IsProducer         bool
+	RxNew              bool
+	ProducerIntervalMs uint32
+	ProducerTimerMs    uint32
+	canmodule          *CANModule
+	TxBuffer           *BufferTxFrame
+	ExtensionEntry1012 Extension
+}
+
+func (time *TIME) Handle(frame can.Frame) {
+	if len(frame.Data) != 6 {
+		return
+	}
+	// Copy frame data to timestamp
+	copy(time.RawTimestamp[:], frame.Data[:])
+	time.RxNew = true
+
+}
+
+func (time *TIME) Init(entry1012 *Entry, canmodule *CANModule, producerIntervalMs uint32) error {
+	if time == nil || entry1012 == nil || canmodule == nil {
+		return CO_ERROR_ILLEGAL_ARGUMENT
+	}
+	// Read param from OD
+	cobIdTimestamp := uint32(0)
+	ret := entry1012.GetUint32(0, &cobIdTimestamp)
+	if ret != nil {
+		return CO_ERROR_OD_PARAMETERS
+	}
+	time.ExtensionEntry1012.Object = time
+	time.ExtensionEntry1012.Read = ReadEntryOriginal
+	time.ExtensionEntry1012.Write = WriteEntry1012
+	entry1012.AddExtension(&time.ExtensionEntry1012)
+
+	cobId := cobIdTimestamp & 0x7FF
+	time.IsConsumer = (cobIdTimestamp & 0x80000000) != 0
+	time.IsProducer = (cobIdTimestamp & 0x40000000) != 0
+	time.RxNew = false
+	log.Infof("[TIME] producer : %v | consumer : %v", time.IsProducer, time.IsConsumer)
+	var err error
+	if time.IsConsumer {
+		_, err = canmodule.InsertRxBuffer(
+			cobId,
+			0x7FF,
+			false,
+			time,
+		)
+		if err != nil {
+			return CO_ERROR_ILLEGAL_ARGUMENT
+		}
+	}
+	time.canmodule = canmodule
+	time.TxBuffer, _, err = canmodule.InsertTxBuffer(
+		cobId,
+		false,
+		6,
+		false,
+	)
+	if time.TxBuffer == nil || err != nil {
+		return CO_ERROR_ILLEGAL_ARGUMENT
+	}
+	// Set internal
+	time.SetInternalTime()
+	time.ProducerIntervalMs = producerIntervalMs
+	time.ProducerTimerMs = producerIntervalMs
+	if time.IsProducer {
+		log.Infof("[TIME] publish period is %v", producerIntervalMs)
+	}
+	return nil
+}
+
+func (time *TIME) Process(nmtIsPreOrOperational bool, timeDifferenceUs uint32) bool {
+	timestampReceived := false
+	if nmtIsPreOrOperational && time.IsConsumer {
+		if time.RxNew {
+			time.Ms = binary.LittleEndian.Uint32(time.RawTimestamp[0:4]) & 0x0FFFFFFF
+			time.Days = binary.LittleEndian.Uint16(time.RawTimestamp[4:6])
+			time.ResidualUs = 0
+			timestampReceived = true
+			time.RxNew = false
+		}
+	} else {
+		time.RxNew = false
+	}
+	ms := uint32(0)
+	if !timestampReceived && (timeDifferenceUs > 0) {
+		us := timeDifferenceUs + uint32(time.ResidualUs)
+		ms = us / 1000
+		time.ResidualUs = uint16(us % 1000)
+		time.Ms += ms
+		if time.Ms >= 1000*60*60*24 {
+			time.Ms -= 1000 * 60 * 60 * 24
+			time.Days += 1
+		}
+	}
+	if nmtIsPreOrOperational && time.IsProducer && time.ProducerIntervalMs > 0 {
+		if time.ProducerTimerMs >= time.ProducerIntervalMs {
+			time.ProducerTimerMs -= time.ProducerIntervalMs
+			binary.LittleEndian.PutUint32(time.TxBuffer.Data[0:4], time.Ms)
+			binary.LittleEndian.PutUint16(time.TxBuffer.Data[4:6], time.Days)
+			time.canmodule.Send(*time.TxBuffer)
+		} else {
+			time.ProducerTimerMs += ms
+		}
+	} else {
+		time.ProducerTimerMs = time.ProducerIntervalMs
+	}
+
+	return timestampReceived
+}
+
+// Sets the internal time
+func (time_obj *TIME) SetInternalTime() {
+	timeBegin := time.Date(1984, time.January, 1, 0, 0, 0, 0, time.Local)
+	duration := time.Since(timeBegin)
+	// Get the total number of days since 1st of jan 1984
+	days := uint16(duration.Hours() / 24)
+	// Get number of milliseconds after midnight
+	midnight := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.Local)
+	ms := time.Since(midnight).Milliseconds()
+	time_obj.ResidualUs = 0
+	time_obj.Ms = uint32(ms)
+	time_obj.Days = days
+	log.Infof("[TIME] days since 01/01/1984 : %v | ms since 00:00 : %v", days, ms)
+}
