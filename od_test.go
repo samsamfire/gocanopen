57380ef55cefd19842b955ce3365bd16a5bd1fc6
samsam <samfire.pilote@gmail.com>
2023-11-15 22:08:02 +0100

    Avoid printing inside od_test

--- od_test.go
+++ od_test.go
@@ -6,8 +6,7 @@ import (
 
 func TestParseEDS(t *testing.T) {
 
-	od, err := ParseEDS("testdata/base.eds", 0x10)
-	od.Print()
+	_, err := ParseEDS("testdata/base.eds", 0x10)
 
 	if err != nil {
 		t.Errorf("Error %s", err)
