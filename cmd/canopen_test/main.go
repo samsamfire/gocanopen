d5825919690f913e6001208d3a3bf3371863047f
samsam <samfire.pilote@gmail.com>
2023-11-26 23:15:18 +0100

    Updated canopen_test

--- cmd/canopen_test/main.go
+++ cmd/canopen_test/main.go
@@ -10,7 +10,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var DEFAULT_NODE_ID = 0x20
+var DEFAULT_NODE_ID = 0x10
 var DEFAULT_CAN_INTERFACE = "can0"
 
 const (
@@ -22,25 +22,23 @@ const (
 func main() {
 	log.SetLevel(log.DebugLevel)
 	// Command line arguments
-	can_interface := flag.String("i", DEFAULT_CAN_INTERFACE, "socketcan interface e.g. can0,vcan0")
-	node_id := flag.Int("n", DEFAULT_NODE_ID, "node id")
 	eds_path := flag.String("p", "", "eds file path")
 	flag.Parse()
 
 	network := canopen.NewNetwork(nil)
-	err := network.Connect("", *can_interface, 500000)
+	err := network.Connect("virtualcan", "127.0.0.1:18888", 500000)
 	if err != nil {
 		panic(err)
 	}
 
 	// Load node EDS, this will be used to generate all the CANopen objects
 	// Basic template can be found in the current directory
-	node, err := network.CreateNode(uint8(*node_id), *eds_path)
+	node, err := network.CreateNode(uint8(DEFAULT_NODE_ID), *eds_path)
 	if err != nil {
 		panic(err)
 	}
 	// Add file extension
-	node.OD.AddFile(0x200F, "File", "example2.bin", os.O_RDONLY|os.O_CREATE, os.O_CREATE|os.O_TRUNC|os.O_WRONLY)
+	node.OD.AddFile(0x200F, "File", "example.bin", os.O_RDONLY|os.O_CREATE, os.O_CREATE|os.O_TRUNC|os.O_WRONLY)
 	if err != nil {
 		panic(err)
 	}
