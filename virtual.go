aeff614f407352c286aeadfcc85af050c5cb8c4f
samsam <samfire.pilote@gmail.com>
2023-11-30 00:27:07 +0100

    Changes: Add write deadline when writing to socket otherwise some messages can get dropped

--- virtual.go
+++ virtual.go
@@ -97,6 +97,7 @@ func (client *VirtualCanBus) Send(frame Frame) error {
 		if err != nil {
 			return err
 		}
+		client.conn.SetWriteDeadline(time.Now().Add(10 * time.Millisecond))
 		_, err = client.conn.Write(frameBytes)
 		return err
 	}
@@ -124,7 +125,7 @@ func (client *VirtualCanBus) Recv() (*Frame, error) {
 	if client.conn == nil {
 		return nil, fmt.Errorf("Error : no active connection, abort receive")
 	}
-	client.conn.SetDeadline(time.Now().Add(200 * time.Millisecond))
+	client.conn.SetReadDeadline(time.Now().Add(200 * time.Millisecond))
 	headerBytes := make([]byte, 4)
 	n, err := client.conn.Read(headerBytes)
 	if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
@@ -135,7 +136,7 @@ func (client *VirtualCanBus) Recv() (*Frame, error) {
 	}
 	length := binary.BigEndian.Uint32(headerBytes)
 	frameBytes := make([]byte, length)
-	client.conn.SetDeadline(time.Now().Add(200 * time.Millisecond))
+	client.conn.SetReadDeadline(time.Now().Add(200 * time.Millisecond))
 	n, err = client.conn.Read(frameBytes)
 	if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
 		return nil, err
