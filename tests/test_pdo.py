189a8a4e0f5a817a329da00d307f4caba57699ee
samsam <samfire.pilote@gmail.com>
2023-11-29 23:29:23 +0100

    Updated PDO tests

--- tests/test_pdo.py
+++ tests/test_pdo.py
@@ -1,10 +1,26 @@
 import logging
+from typing import List
+import time
 
 import pytest
 import canopen
+from canopen import RemoteNode
 from canopen.pdo.base import Map
 
 logger = logging.getLogger(__name__)
+
+VARIABLES_TO_MAP = [
+    "UNSIGNED8",
+    "UNSIGNED16",
+    "UNSIGNED32",
+    "UNSIGNED64",
+    "INTEGER8",
+    "INTEGER16",
+    "INTEGER32",
+    "INTEGER64",
+    "REAL32",
+    "REAL64",
+]
 
 
 @pytest.fixture(scope="module")
@@ -23,48 +39,43 @@ def first_rpdo(node_configured) -> Map:
     return node_configured.rpdo[1]
 
 
+@pytest.fixture()
+def first_pdos(first_rpdo, first_tpdo) -> List[Map]:
+    return [first_rpdo, first_tpdo]
+
+
+def test_map_pdo(node_configured):
+    for pdo in node_configured.pdo.values():
+        for variable in VARIABLES_TO_MAP:
+            pdo.clear()
+            pdo.add_variable(f"{variable} value")
+            pdo.save()
+            pdo.read()
+            assert pdo[f"{variable} value"] is not None
+
+
+def test_map_pdo_multiple(first_pdos: List[Map]):
+    for pdo in first_pdos:
+        pdo.clear()
+        for _ in range(8):
+            pdo.add_variable("UNSIGNED8 value")
+        pdo.save()
+        pdo.clear()
+        for _ in range(4):
+            pdo.add_variable("UNSIGNED16 value")
+        pdo.save()
+        pdo.clear()
+        for _ in range(2):
+            pdo.add_variable("UNSIGNED32 value")
+        pdo.save()
+
+
 def test_disable_pdo(first_rpdo: Map, first_tpdo: Map):
-    # Configuring a pdo that is enabled should throw an exception
-    first_tpdo.clear()
-    first_tpdo.enabled = False
-    first_tpdo.save()
-    assert not first_tpdo.enabled
-    first_rpdo.enabled = False
-    first_tpdo.clear()
-    first_rpdo.save()
-    assert not first_rpdo.enabled
-
-
-VARIABLES_TO_MAP = [
-    "UNSIGNED8",
-    "UNSIGNED16",
-    "UNSIGNED32",
-    "UNSIGNED64",
-    "INTEGER8",
-    "INTEGER16",
-    "INTEGER32",
-    "INTEGER64",
-    "REAL32",
-    "REAL64",
-]
-
-
-def test_map_tpdo(first_tpdo: Map):
-    for variable in VARIABLES_TO_MAP:
-        first_tpdo.clear()
-        first_tpdo.add_variable(f"{variable} value")
-        first_tpdo.save()
-        # first_tpdo.read()
-        assert first_tpdo[f"{variable} value"] is not None
-
-
-def test_map_rpdo(first_rpdo: Map):
-    for variable in VARIABLES_TO_MAP:
-        first_rpdo.clear()
-        first_rpdo.add_variable(f"{variable} value")
-        first_rpdo.save()
-        first_rpdo.read()
-        assert first_rpdo[f"{variable} value"] is not None
+    for pdo in [first_tpdo, first_rpdo]:
+        pdo.enabled = False
+        pdo.save()
+        pdo.read()
+        assert pdo.enabled == False
 
 
 def test_map_pdo_invalid_length(first_tpdo: Map):
@@ -90,27 +101,23 @@ def test_tpdo_transmission(first_tpdo: Map):
     assert first_tpdo.wait_for_reception(timeout=1) is not None
 
 
-import time
-
-
 def test_rpdo_receive(node_configured: canopen.RemoteNode, first_rpdo: Map):
     first_rpdo.clear()
     first_rpdo.add_variable("REAL64 value")
-    # first_rpdo.trans_type = 1
+    first_rpdo.trans_type = 1
     first_rpdo.enabled = True
     first_rpdo.cob_id = 0x310
     first_rpdo.save()
     first_rpdo["REAL64 value"].raw = 1.554
-    for i in range(10):
-        time.sleep(0.1)
-        first_rpdo.transmit()
+    first_rpdo.transmit()
+    time.sleep(0.2)
     assert node_configured.sdo["REAL64 value"].raw == 1.554
 
 
 def test_rpdo_receive_consistency(node_configured: canopen.RemoteNode, first_rpdo: Map):
     first_rpdo.clear()
     first_rpdo.add_variable("INTEGER64 value")
-    # first_rpdo.trans_type = 1
+    first_rpdo.trans_type = 1
     first_rpdo.enabled = True
     first_rpdo.cob_id = 0x310
     first_rpdo.save()
@@ -130,13 +137,12 @@ def test_rpdo_receive_consistency(node_configured: canopen.RemoteNode, first_rpd
 
 
 def test_tpdo_receive_consistency(node_configured: canopen.RemoteNode, first_tpdo: Map):
+    received_bytes = []
+
     def tpdo_receiver(map: Map):
         raw_val = map["UNSIGNED64 value"].raw
-        assert (
-            raw_val == 0xAA_AA_AA_AA_AA_AA_AA_A or raw_val == 0xBB_BB_BB_BB_BB_BB_BB_B
-        )
+        received_bytes.append(raw_val)
 
-    first_tpdo.clear()
     first_tpdo.clear()
     first_tpdo.add_variable("UNSIGNED64 value")
     node_configured.sdo["Communication cycle period"].raw = 1000
@@ -144,51 +150,65 @@ def test_tpdo_receive_consistency(node_configured: canopen.RemoteNode, first_tpd
     first_tpdo.enabled = True
     first_tpdo.cob_id = 0x190
     first_tpdo.save()
-    first_tpdo["UNSIGNED64 value"].raw = 8989
     first_tpdo.add_callback(tpdo_receiver)
-    for i in range(100):
-        node_configured.sdo["UNSIGNED64 value"].raw = 0xAA_AA_AA_AA_AA_AA_AA_A
-        node_configured.sdo["UNSIGNED64 value"].raw = 0xBB_BB_BB_BB_BB_BB_BB_B
+    toggle: bool = True
+    for _ in range(100):
+        if toggle:
+            node_configured.sdo["UNSIGNED64 value"].raw = 0xAA_AA_AA_AA_AA_AA_AA_A
+            toggle = False
+        else:
+            node_configured.sdo["UNSIGNED64 value"].raw = 0xBB_BB_BB_BB_BB_BB_BB_B
+            toggle = True
+
+    for raw_val in received_bytes:
+        assert (
+            raw_val == 0xAA_AA_AA_AA_AA_AA_AA_A or raw_val == 0xBB_BB_BB_BB_BB_BB_BB_B
+        )
 
 
-# def test_tpdo_transmission_type(first)
+def test_tpdo_transmission_type(first_tpdo: Map, node_configured: RemoteNode):
+    first_tpdo.clear()
+    first_tpdo.enabled = True
+    first_tpdo.trans_type = 1
+    first_tpdo.add_variable("UNSIGNED64 value")
+    first_tpdo.save()
+    SYNC_PERIOD_S = 0.01
+    # Put a sync value of 0.01s (convert in Us)
+    node_configured.sdo.download(
+        0x1006, 0x0, int(SYNC_PERIOD_S * 1e6).to_bytes(length=4, byteorder="little")
+    )
+    # Check delta between pdo reception
+    timestamp = first_tpdo.wait_for_reception(timeout=0.1)
+    for _ in range(5):
+        new_timestamp = first_tpdo.wait_for_reception(timeout=0.1)
+        assert abs(new_timestamp - timestamp) == pytest.approx(
+            SYNC_PERIOD_S * first_tpdo.trans_type, abs=1e-3
+        )
+        timestamp = new_timestamp
+
+    # Change transmission type to be 5 (every 5th sync)
+    first_tpdo.trans_type = 5
+    first_tpdo.save()
+    first_tpdo.read()
+    assert first_tpdo.trans_type == 5
+    timestamp = first_tpdo.wait_for_reception(timeout=0.1)
+    for i in range(5):
+        new_timestamp = first_tpdo.wait_for_reception(timeout=0.1)
+        assert abs(new_timestamp - timestamp) == pytest.approx(
+            SYNC_PERIOD_S * first_tpdo.trans_type, abs=1e-2
+        )
+        timestamp = new_timestamp
 
 
-# def test_tpdo_communication_parameter(remote_node: canopen.RemoteNode):
-#     pdo = remote_node.tpdo[1]
-#     # Test enable transmission
-#     pdo.enabled = True
-#     pdo.save()
-#     timestamp = pdo.wait_for_reception(timeout=1)
-#     assert timestamp != None
-#     # Test disable transmission
-#     pdo.enabled = False
-#     pdo.save()
-#     timestamp = pdo.wait_for_reception(timeout=1)
-#     assert timestamp == None
-
-
-# def test_tpdo_transmission_type(remote_node: canopen.RemoteNode):
-#     pdo = remote_node.tpdo[1]
-#     # Enable tpdo
-#     pdo.enabled = True
-#     pdo.trans_type = 1
-#     pdo.save()
-#     SYNC_PERIOD = 10000 / 1e6
-#     # Put a sync value of 0.01s (in Us)
-#     remote_node.sdo.download(0x1006, 0x0, int(10000).to_bytes(length=4, byteorder="little"))
-#     # Check delta between pdo reception
-#     timestamp = pdo.wait_for_reception(timeout=0.1)
-#     for i in range(5):
-#         new_timestamp = pdo.wait_for_reception(timeout=0.1)
-#         assert abs(new_timestamp - timestamp) == pytest.approx(SYNC_PERIOD * pdo.trans_type, abs=1e-3)
-#         timestamp = new_timestamp
-
-#     # Change transmission type to be 5 (every 5th sync)
-#     pdo.trans_type = 5
-#     pdo.save()
-#     timestamp = pdo.wait_for_reception(timeout=0.1)
-#     for i in range(5):
-#         new_timestamp = pdo.wait_for_reception(timeout=0.1)
-#         assert abs(new_timestamp - timestamp) == pytest.approx(SYNC_PERIOD * pdo.trans_type, abs=1e-2)
-#         timestamp = new_timestamp
+def test_tpdo_transmission_type(first_tpdo: Map):
+    first_tpdo.clear()
+    first_tpdo.add_variable("UNSIGNED64 value")
+    first_tpdo.enabled = True
+    first_tpdo.inhibit_time = 11000
+    first_tpdo.event_timer = 9999
+    first_tpdo.trans_type = 188
+    first_tpdo.save()
+    first_tpdo.read()
+    assert first_tpdo.inhibit_time == 11000
+    assert first_tpdo.event_timer == 9999
+    assert first_tpdo.trans_type == 188
