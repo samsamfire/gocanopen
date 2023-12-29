92951c5ccaabbb6dc94b654d5cd792a3a4665082
Samuel Lee <samfire.pilote@gmail.com>
2023-12-30 00:41:03 +0100

    Adding Remote node, untested for now

---
+++ node_remote_test.go
@@ -0,0 +1,22 @@
+package canopen
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateRemoteNode(t *testing.T) {
+	network := createNetwork()
+	networkRemote := createNetworkEmpty()
+	defer network.Disconnect()
+	defer networkRemote.Disconnect()
+	node, err := networkRemote.AddNode(NODE_ID_TEST, "testdata/base.eds")
+	assert.Nil(t, err)
+	assert.NotNil(t, node)
+	err = node.InitPDOs(true)
+	assert.Nil(t, err, err)
+	// err = node.InitPDOs(true)
+	// assert.Nil(t, err, err)
+
+}
