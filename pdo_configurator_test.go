d2560425f48d267ed27dc0fa5c29d8582362892a
Samuel Lee <samfire.pilote@gmail.com>
2023-12-29 16:32:27 +0100

    Added WriteConfiguration and ReadEnabled to pdo configurator

--- pdo_configurator_test.go
+++ pdo_configurator_test.go
@@ -6,6 +6,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var TEST_MAPPING = []PDOMapping{
+	{Index: 0x2001, Subindex: 0x0, LengthBits: 8},
+	{Index: 0x2002, Subindex: 0x0, LengthBits: 8},
+	{Index: 0x2003, Subindex: 0x0, LengthBits: 16},
+	{Index: 0x2004, Subindex: 0x0, LengthBits: 32},
+}
+
 // Test everything common to RPDO & TPDO
 func TestPDOConfiguratorCommon(t *testing.T) {
 	pdoNb := uint16(1)
@@ -26,22 +33,33 @@ func TestPDOConfiguratorCommon(t *testing.T) {
 		assert.EqualValues(t, 11, transType)
 		err = conf.Disable(pdoNb)
 		assert.Nil(t, err)
+		enabled, _ := conf.ReadEnabled(pdoNb)
+		assert.Equal(t, false, enabled)
 		err = conf.ClearMappings(pdoNb)
 		assert.Nil(t, err)
-		mappings := []PDOMapping{
-			{Index: 0x2001, Subindex: 0x0, LengthBits: 8},
-			{Index: 0x2002, Subindex: 0x0, LengthBits: 8},
-			{Index: 0x2003, Subindex: 0x0, LengthBits: 16},
-			{Index: 0x2004, Subindex: 0x0, LengthBits: 32},
-		}
-		err = conf.UpdateMappings(pdoNb, mappings)
+		err = conf.WriteMappings(pdoNb, TEST_MAPPING)
 		assert.Nil(t, err)
 		mappingsFdbk, _ := conf.ReadMappings(pdoNb)
-		assert.Equal(t, mappings, mappingsFdbk)
+		assert.Equal(t, TEST_MAPPING, mappingsFdbk)
 		err = conf.WriteCanId(pdoNb, 0x211)
 		assert.Nil(t, err)
 		cobId, _ := conf.ReadCobId(pdoNb)
 		assert.EqualValues(t, 0x211, cobId&0x7FF)
+		config := PDOConfiguration{
+			CanId:            0x255,
+			TransmissionType: 22,
+			EventTimer:       1400,
+			InhibitTime:      1200,
+			Mappings:         TEST_MAPPING,
+		}
+		err = conf.WriteConfiguration(pdoNb, config)
+		assert.Nil(t, err)
+		readConfig, _ := conf.ReadConfiguration(pdoNb)
+		assert.EqualValues(t, config.CanId, readConfig.CanId)
+		assert.EqualValues(t, config.TransmissionType, readConfig.TransmissionType)
+		assert.EqualValues(t, config.EventTimer, readConfig.EventTimer)
+		assert.EqualValues(t, config.Mappings, readConfig.Mappings)
+
 		err = conf.Enable(pdoNb)
 		assert.Nil(t, err)
 	}
