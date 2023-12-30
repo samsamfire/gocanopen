0043fb7cf5b3f7fc2d4c7b218ee95a4c21d0c740
Samuel Lee <samfire.pilote@gmail.com>
2023-12-30 22:56:45 +0100

    New test for remote Node passing : listen to a TPDO and update internal OD

--- node_remote_test.go
+++ node_remote_test.go
@@ -2,6 +2,7 @@ package canopen
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -18,21 +19,21 @@ func TestCreateRemoteNode(t *testing.T) {
 	assert.Nil(t, err, err)
 }
 
-// func TestRemoteNodeRPDO(t *testing.T) {
-// 	network := createNetwork()
-// 	networkRemote := createNetworkEmpty()
-// 	defer network.Disconnect()
-// 	defer networkRemote.Disconnect()
-// 	node, err := networkRemote.AddNode(NODE_ID_TEST, "testdata/base.eds", true)
-// 	assert.Nil(t, err)
-// 	assert.NotNil(t, node)
-// 	err = node.InitPDOs(true)
-// 	assert.Nil(t, err, err)
-// 	err = network.WriteRaw(NODE_ID_TEST, 0x2002, 0, []byte{10})
-// 	assert.Nil(t, err)
-// 	time.Sleep(1 * time.Second)
-// 	read := make([]byte, 1)
-// 	node.sdoClient.ReadRaw(0, 0x2002, 0x0, read)
-// 	assert.Equal(t, NODE_RUNNING, node.GetState())
-// 	assert.Equal(t, []byte{10}, read)
-// }
+func TestRemoteNodeRPDO(t *testing.T) {
+	network := createNetwork()
+	networkRemote := createNetworkEmpty()
+	defer network.Disconnect()
+	defer networkRemote.Disconnect()
+	remoteNode, err := networkRemote.AddNode(NODE_ID_TEST, "testdata/base.eds", true)
+	configurator := network.Configurator(NODE_ID_TEST)
+	configurator.TPDO.Enable(1)
+	assert.Nil(t, err)
+	assert.NotNil(t, remoteNode)
+	err = network.WriteRaw(NODE_ID_TEST, 0x2002, 0, []byte{10})
+	assert.Nil(t, err)
+	time.Sleep(200 * time.Millisecond)
+	read := make([]byte, 1)
+	remoteNode.sdoClient.ReadRaw(0, 0x2002, 0x0, read)
+	assert.Equal(t, NODE_RUNNING, remoteNode.GetState())
+	assert.Equal(t, []byte{10}, read)
+}
