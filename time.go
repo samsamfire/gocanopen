c89cb7f84185eb5af4ec09729ada061985f219f3
Samuel Lee <samfire.pilote@gmail.com>
2024-01-03 15:00:50 +0100

    Unexported everything inside of TIME

--- time.go
+++ time.go
@@ -9,15 +9,15 @@ import (
 
 type TIME struct {
 	*busManager
-	RawTimestamp       [6]byte
-	Ms                 uint32 // Milliseconds after midnight
-	Days               uint16 // Days since 1st january 1984
-	ResidualUs         uint16 // Residual Us calculated when processed
-	IsConsumer         bool
-	IsProducer         bool
-	RxNew              bool
-	ProducerIntervalMs uint32
-	ProducerTimerMs    uint32
+	rawTimestamp       [6]byte
+	ms                 uint32 // Milliseconds after midnight
+	days               uint16 // Days since 1st january 1984
+	residualUs         uint16 // Residual Us calculated when processed
+	isConsumer         bool
+	isProducer         bool
+	rxNew              bool
+	producerIntervalMs uint32
+	producerTimerMs    uint32
 	cobId              uint32
 }
 
@@ -25,47 +25,47 @@ func (time *TIME) Handle(frame Frame) {
 	if len(frame.Data) != 6 {
 		return
 	}
-	copy(time.RawTimestamp[:], frame.Data[:])
-	time.RxNew = true
+	copy(time.rawTimestamp[:], frame.Data[:])
+	time.rxNew = true
 
 }
 
 func (time *TIME) process(nmtIsPreOrOperational bool, timeDifferenceUs uint32) bool {
 	timestampReceived := false
-	if nmtIsPreOrOperational && time.IsConsumer {
-		if time.RxNew {
-			time.Ms = binary.LittleEndian.Uint32(time.RawTimestamp[0:4]) & 0x0FFFFFFF
-			time.Days = binary.LittleEndian.Uint16(time.RawTimestamp[4:6])
-			time.ResidualUs = 0
+	if nmtIsPreOrOperational && time.isConsumer {
+		if time.rxNew {
+			time.ms = binary.LittleEndian.Uint32(time.rawTimestamp[0:4]) & 0x0FFFFFFF
+			time.days = binary.LittleEndian.Uint16(time.rawTimestamp[4:6])
+			time.residualUs = 0
 			timestampReceived = true
-			time.RxNew = false
+			time.rxNew = false
 		}
 	} else {
-		time.RxNew = false
+		time.rxNew = false
 	}
 	ms := uint32(0)
 	if !timestampReceived && (timeDifferenceUs > 0) {
-		us := timeDifferenceUs + uint32(time.ResidualUs)
+		us := timeDifferenceUs + uint32(time.residualUs)
 		ms = us / 1000
-		time.ResidualUs = uint16(us % 1000)
-		time.Ms += ms
-		if time.Ms >= 1000*60*60*24 {
-			time.Ms -= 1000 * 60 * 60 * 24
-			time.Days += 1
+		time.residualUs = uint16(us % 1000)
+		time.ms += ms
+		if time.ms >= 1000*60*60*24 {
+			time.ms -= 1000 * 60 * 60 * 24
+			time.days += 1
 		}
 	}
-	if nmtIsPreOrOperational && time.IsProducer && time.ProducerIntervalMs > 0 {
-		if time.ProducerTimerMs >= time.ProducerIntervalMs {
-			time.ProducerTimerMs -= time.ProducerIntervalMs
+	if nmtIsPreOrOperational && time.isProducer && time.producerIntervalMs > 0 {
+		if time.producerTimerMs >= time.producerIntervalMs {
+			time.producerTimerMs -= time.producerIntervalMs
 			frame := NewFrame(time.cobId, 0, 6)
-			binary.LittleEndian.PutUint32(frame.Data[0:4], time.Ms)
-			binary.LittleEndian.PutUint16(frame.Data[4:6], time.Days)
+			binary.LittleEndian.PutUint32(frame.Data[0:4], time.ms)
+			binary.LittleEndian.PutUint16(frame.Data[4:6], time.days)
 			time.Send(frame)
 		} else {
-			time.ProducerTimerMs += ms
+			time.producerTimerMs += ms
 		}
 	} else {
-		time.ProducerTimerMs = time.ProducerIntervalMs
+		time.producerTimerMs = time.producerIntervalMs
 	}
 
 	return timestampReceived
@@ -80,9 +80,9 @@ func (time_obj *TIME) SetInternalTime() {
 	// Get number of milliseconds after midnight
 	midnight := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.Local)
 	ms := time.Since(midnight).Milliseconds()
-	time_obj.ResidualUs = 0
-	time_obj.Ms = uint32(ms)
-	time_obj.Days = days
+	time_obj.residualUs = 0
+	time_obj.ms = uint32(ms)
+	time_obj.days = days
 	log.Infof("[TIME] setting the date to %v", time.Now())
 	log.Infof("[TIME] days since 01/01/1984 : %v | ms since 00:00 : %v", days, ms)
 }
@@ -100,21 +100,21 @@ func NewTIME(bm *busManager, entry1012 *Entry, producerIntervalMs uint32) (*TIME
 	}
 	entry1012.AddExtension(time, ReadEntryDefault, writeEntry1012)
 	cobId := cobIdTimestamp & 0x7FF
-	time.IsConsumer = (cobIdTimestamp & 0x80000000) != 0
-	time.IsProducer = (cobIdTimestamp & 0x40000000) != 0
-	time.RxNew = false
+	time.isConsumer = (cobIdTimestamp & 0x80000000) != 0
+	time.isProducer = (cobIdTimestamp & 0x40000000) != 0
+	time.rxNew = false
 	time.cobId = cobId
-	if time.IsConsumer {
+	if time.isConsumer {
 		err := bm.Subscribe(cobId, 0x7FF, false, time)
 		if err != nil {
 			return nil, ErrIllegalArgument
 		}
 	}
 	time.SetInternalTime()
-	time.ProducerIntervalMs = producerIntervalMs
-	time.ProducerTimerMs = producerIntervalMs
-	log.Infof("[TIME] initialized time object | producer : %v, consumer : %v", time.IsProducer, time.IsConsumer)
-	if time.IsProducer {
+	time.producerIntervalMs = producerIntervalMs
+	time.producerTimerMs = producerIntervalMs
+	log.Infof("[TIME] initialized time object | producer : %v, consumer : %v", time.isProducer, time.isConsumer)
+	if time.isProducer {
 		log.Infof("[TIME] publish period is %v ms", producerIntervalMs)
 	}
 	return time, err
