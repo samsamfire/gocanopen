959b58415f24972d9316b7552a1a27cf49815746
Samuel Lee <samfire.pilote@gmail.com>
2024-03-23 13:46:45 +0100

    Added test for set internal time and moved time origin as a global var

---
+++ pkg/time/time_test.go
@@ -0,0 +1,19 @@
+package time
+
+import (
+	"testing"
+	"time"
+
+	// import assert
+	"github.com/stretchr/testify/assert"
+)
+
+var test = time.Time{}
+
+func TestSetInternalTime(t *testing.T) {
+
+	timeInstance := &TIME{}
+	timeInstance.SetInternalTime()
+	expectedDays := uint16(time.Since(timestampOrigin).Hours() / 24)
+	assert.Equal(t, timeInstance.days, expectedDays)
+}
