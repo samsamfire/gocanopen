3acd2df5fb85623617c4a55a7fbb3c65a347a315
Samuel Lee <samfire.pilote@gmail.com>
2024-08-11 13:31:51 +0200

    Fixed/Changed : added kvaser build tag to not compile for kvaser by default, added structure that knows what interfaces exist, even if not compiled to display specific error message

--- pkg/can/kvaser/kvaser.go
+++ pkg/can/kvaser/kvaser.go
@@ -1,3 +1,5 @@
+//go:build kvaser
+
 package kvaser
 
 /*
--- pkg/can/register.go
+++ pkg/can/register.go
@@ -4,10 +4,16 @@ import canopen "github.com/samsamfire/gocanopen"
 
 type NewInterfaceFunc func(channel string) (canopen.Bus, error)
 
-var CanInterfaces = make(map[string]NewInterfaceFunc)
+var AvailableInterfaces = make(map[string]NewInterfaceFunc)
+var ImplementedInterfaces = []string{
+	"socketcan",
+	"socketcanv2",
+	"virtualcan",
+	"kvaser",
+}
 
 // Register a new CAN bus interface type
 // This should be called inside an init() function of plugin
 func RegisterInterface(interfaceType string, newInterface NewInterfaceFunc) {
-	CanInterfaces[interfaceType] = newInterface
+	AvailableInterfaces[interfaceType] = newInterface
 }
