e10d2e26730d2da1495578221adadebd1755202b
samsam <samfire.pilote@gmail.com>
2024-12-09 22:05:53 +0100

    Fixed : error in gateway example, should now work + slog

--- examples/http/main.go
+++ examples/http/main.go
@@ -3,34 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/samsamfire/gocanopen/pkg/gateway/http"
 	"github.com/samsamfire/gocanopen/pkg/network"
-	log "github.com/sirupsen/logrus"
 )
 
-var DEFAULT_NODE_ID = 0x20
-var DEFAULT_CAN_INTERFACE = "vcan0"
-var DEFAULT_HTTP_PORT = 8090
-
 const (
-	INIT     = 0
-	RUNNING  = 1
-	RESETING = 2
+	NodeId    = 0x20
+	Interface = "vcan0"
+	Port      = 8090
 )
 
 func main() {
-	log.SetLevel(log.DebugLevel)
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	// Command line arguments
-	channel := flag.String("i", DEFAULT_CAN_INTERFACE, "socketcan channel e.g. can0,vcan0")
+	channel := flag.String("i", Interface, "socketcan channel e.g. can0,vcan0")
 	flag.Parse()
 
 	network := network.NewNetwork(nil)
-	e := network.Connect("", *channel, 500000)
+	network.SetLogger(logger)
+	e := network.Connect("socketcan", *channel, 500000)
 	if e != nil {
 		panic(e)
 	}
-	gateway := http.NewGatewayServer(&network, 1, 1, 1000)
-	gateway.ListenAndServe(fmt.Sprintf(":%d", DEFAULT_HTTP_PORT))
+	gateway := http.NewGatewayServer(&network, logger, 1, 1, 1000)
+	gateway.ListenAndServe(fmt.Sprintf(":%d", Port))
 
 }
