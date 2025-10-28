10cfebce5b0d4fed4f6bb80c7561089adf0dc705
Samuel Lee <samfire.pilote@gmail.com>
2025-10-28 09:56:36 +0100

    perf: improved local OD reads by a significant margin (>10x)

--- pkg/od/variable.go
+++ pkg/od/variable.go
@@ -3,7 +3,6 @@ package od
 import (
 	"encoding/binary"
 	"math"
-	"strconv"
 	"sync"
 )
 
@@ -59,324 +58,204 @@ func NewVariable(
 }
 
 // Return number of bytes
-func (variable *Variable) DataLength() uint32 {
-	return uint32(len(variable.value))
+func (v *Variable) DataLength() uint32 {
+	return uint32(len(v.value))
 }
 
 // Return default value as byte slice
-func (variable *Variable) DefaultValue() []byte {
-	return variable.valueDefault
+func (v *Variable) DefaultValue() []byte {
+	return v.valueDefault
 }
 
-// EncodeFromString value from EDS into bytes respecting canopen datatype
-func EncodeFromString(value string, datatype uint8, offset uint8) ([]byte, error) {
+// Return value as uint64 (only for UNSIGNED types)
+func (v *Variable) Uint() (uint64, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
 
-	var data []byte
-	var err error
-	var parsedInt int64
-	var parsedUint uint64
-
-	if value == "" {
-		// Treat empty string as a 0 value
-		value = "0"
-	}
-
-	switch datatype {
-	case BOOLEAN, UNSIGNED8:
-		parsedUint, err = strconv.ParseUint(value, 0, 8)
-		data = []byte{byte(uint8(parsedUint + uint64(offset)))}
-
-	case INTEGER8:
-		parsedInt, err = strconv.ParseInt(value, 0, 8)
-		data = []byte{byte(parsedInt + int64(offset))}
-
+	switch v.DataType {
+	case UNSIGNED8:
+		return uint64(v.value[0]), nil
 	case UNSIGNED16:
-		parsedUint, err = strconv.ParseUint(value, 0, 16)
-		data = make([]byte, 2)
-		binary.LittleEndian.PutUint16(data, uint16(parsedUint+uint64(offset)))
-
-	case INTEGER16:
-		parsedInt, err = strconv.ParseInt(value, 0, 16)
-		data = make([]byte, 2)
-		binary.LittleEndian.PutUint16(data, uint16(parsedInt+int64(offset)))
-
+		return uint64(binary.LittleEndian.Uint16(v.value)), nil
 	case UNSIGNED32:
-		parsedUint, err = strconv.ParseUint(value, 0, 32)
-		data = make([]byte, 4)
-		binary.LittleEndian.PutUint32(data, uint32(parsedUint+uint64(offset)))
-
-	case INTEGER32:
-		parsedInt, err = strconv.ParseInt(value, 0, 32)
-		data = make([]byte, 4)
-		binary.LittleEndian.PutUint32(data, uint32(parsedInt+int64(offset)))
-
-	case REAL32:
-		var parsedFloat float64
-		parsedFloat, err = strconv.ParseFloat(value, 32)
-		data = make([]byte, 4)
-		binary.LittleEndian.PutUint32(data, math.Float32bits(float32(parsedFloat)))
-
+		return uint64(binary.LittleEndian.Uint32(v.value)), nil
 	case UNSIGNED64:
-		parsedUint, err = strconv.ParseUint(value, 0, 64)
-		data = make([]byte, 8)
-		binary.LittleEndian.PutUint64(data, parsedUint+uint64(offset))
-
-	case INTEGER64:
-		parsedInt, err = strconv.ParseInt(value, 0, 64)
-		data = make([]byte, 8)
-		binary.LittleEndian.PutUint64(data, uint64(parsedInt+int64(offset)))
-
-	case REAL64:
-		var parsedFloat float64
-		parsedFloat, err = strconv.ParseFloat(value, 64)
-		data = make([]byte, 8)
-		binary.LittleEndian.PutUint64(data, math.Float64bits(parsedFloat))
-
-	case VISIBLE_STRING, OCTET_STRING:
-		return []byte(value), nil
-
-	case DOMAIN:
-		return []byte{}, nil
-
+		return binary.LittleEndian.Uint64(v.value), nil
 	default:
-		return nil, ErrTypeMismatch
-
+		return 0, ErrTypeMismatch
 	}
-	return data, err
 }
 
-// Encode from generic type
-func EncodeFromGeneric(data any) ([]byte, error) {
-	var encoded []byte
-	switch val := data.(type) {
-	case uint8:
-		encoded = []byte{val}
-	case int8:
-		encoded = []byte{byte(val)}
-	case uint16:
-		encoded = make([]byte, 2)
-		binary.LittleEndian.PutUint16(encoded, val)
-	case int16:
-		encoded = make([]byte, 2)
-		binary.LittleEndian.PutUint16(encoded, uint16(val))
-	case uint32:
-		encoded = make([]byte, 4)
-		binary.LittleEndian.PutUint32(encoded, val)
-	case int32:
-		encoded = make([]byte, 4)
-		binary.LittleEndian.PutUint32(encoded, uint32(val))
-	case uint64:
-		encoded = make([]byte, 8)
-		binary.LittleEndian.PutUint64(encoded, val)
-	case int64:
-		encoded = make([]byte, 8)
-		binary.LittleEndian.PutUint64(encoded, uint64(val))
-	case string:
-		encoded = []byte(val)
-	case float32:
-		encoded = make([]byte, 4)
-		binary.LittleEndian.PutUint32(encoded, math.Float32bits(val))
-	case float64:
-		encoded = make([]byte, 8)
-		binary.LittleEndian.PutUint64(encoded, math.Float64bits(val))
-	case []byte:
-		encoded = val
-	default:
-		return nil, ErrTypeMismatch
+func (v *Variable) Uint8() (uint8, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != UNSIGNED8 {
+		return 0, ErrTypeMismatch
 	}
-	return encoded, nil
+	return v.value[0], nil
 }
 
-// Helper function for checking consistency between size and datatype
-func CheckSize(length int, dataType uint8) error {
-	switch dataType {
-	case BOOLEAN, UNSIGNED8, INTEGER8:
-		if length < 1 {
-			return ErrDataShort
-		} else if length > 1 {
-			return ErrDataLong
-		}
-	case UNSIGNED16, INTEGER16:
-		if length < 2 {
-			return ErrDataShort
-		} else if length > 2 {
-			return ErrDataLong
-		}
+func (v *Variable) Uint16() (uint16, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
 
-	case UNSIGNED32, INTEGER32, REAL32:
-		if length < 4 {
-			return ErrDataShort
-		} else if length > 4 {
-			return ErrDataLong
-		}
-	case UNSIGNED64, INTEGER64, REAL64:
-		if length < 8 {
-			return ErrDataShort
-		} else if length > 8 {
-			return ErrDataLong
-		}
-	// All other datatypes, no size check
-	default:
-		return nil
+	if v.DataType != UNSIGNED16 {
+		return 0, ErrTypeMismatch
 	}
-	return nil
-
+	return binary.LittleEndian.Uint16(v.value), nil
 }
 
-// Decode byte array given the CANopen data type
-// Function will return either string, int64, uint64, or float64
-func DecodeToType(data []byte, dataType uint8) (v any, e error) {
-	e = CheckSize(len(data), dataType)
-	if e != nil {
-		return nil, e
+func (v *Variable) Uint32() (uint32, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != UNSIGNED32 {
+		return 0, ErrTypeMismatch
 	}
-	// Cast to correct type
-	switch dataType {
-	case BOOLEAN, UNSIGNED8:
-		return uint64(data[0]), nil
+	return binary.LittleEndian.Uint32(v.value), nil
+}
+
+func (v *Variable) Uint64() (uint64, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != UNSIGNED64 {
+		return 0, ErrTypeMismatch
+	}
+	return binary.LittleEndian.Uint64(v.value), nil
+}
+
+// Return value as int64 (only for SIGNED types)
+func (v *Variable) Int() (int64, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	switch v.DataType {
 	case INTEGER8:
-		return int64(data[0]), nil
-	case UNSIGNED16:
-		return uint64(binary.LittleEndian.Uint16(data)), nil
+		return int64(int8(v.value[0])), nil
 	case INTEGER16:
-		return int64(int16(binary.LittleEndian.Uint16(data))), nil
-	case UNSIGNED32:
-		return uint64(binary.LittleEndian.Uint32(data)), nil
+		return int64(int16(binary.LittleEndian.Uint16(v.value))), nil
 	case INTEGER32:
-		return int64(int32(binary.LittleEndian.Uint32(data))), nil
-	case UNSIGNED64:
-		return uint64(binary.LittleEndian.Uint64(data)), nil
+		return int64(int32(binary.LittleEndian.Uint32(v.value))), nil
 	case INTEGER64:
-		return int64(binary.LittleEndian.Uint64(data)), nil
+		return int64(binary.LittleEndian.Uint64(v.value)), nil
+	default:
+		return 0, ErrTypeMismatch
+	}
+}
+func (v *Variable) Int8() (int8, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != INTEGER8 {
+		return 0, ErrTypeMismatch
+	}
+	return int8(v.value[0]), nil
+}
+
+func (v *Variable) Int16() (int16, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != INTEGER16 {
+		return 0, ErrTypeMismatch
+	}
+	return int16(binary.LittleEndian.Uint16(v.value)), nil
+}
+
+func (v *Variable) Int32() (int32, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != INTEGER32 {
+		return 0, ErrTypeMismatch
+	}
+	return int32(binary.LittleEndian.Uint32(v.value)), nil
+}
+
+func (v *Variable) Int64() (int64, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != INTEGER64 {
+		return 0, ErrTypeMismatch
+	}
+	return int64(binary.LittleEndian.Uint64(v.value)), nil
+}
+
+// Return value as float64 (only for REAL types)
+func (v *Variable) Float() (float64, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	switch v.DataType {
 	case REAL32:
-		parsed := binary.LittleEndian.Uint32(data)
+		parsed := binary.LittleEndian.Uint32(v.value)
 		return float64(math.Float32frombits(parsed)), nil
 	case REAL64:
-		parsed := binary.LittleEndian.Uint64(data)
+		parsed := binary.LittleEndian.Uint64(v.value)
 		return math.Float64frombits(parsed), nil
-	case VISIBLE_STRING, OCTET_STRING:
-		return string(data), nil
-	case DOMAIN:
-		return int64(0), nil
 	default:
-		return nil, ErrTypeMismatch
+		return 0, ErrTypeMismatch
 	}
 }
 
-// Decode byte array given the CANopen data type
-// Function will return the exact type (uint8,uint16,...,int8,...)
-func DecodeToTypeExact(data []byte, dataType uint8) (v any, e error) {
-	e = CheckSize(len(data), dataType)
-	if e != nil {
-		return nil, e
+func (v *Variable) Float32() (float32, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != REAL32 {
+		return 0, ErrTypeMismatch
 	}
-	// Cast to correct type
-	switch dataType {
-	case BOOLEAN, UNSIGNED8:
-		return data[0], nil
-	case INTEGER8:
-		return int8(data[0]), nil
-	case UNSIGNED16:
-		return binary.LittleEndian.Uint16(data), nil
-	case INTEGER16:
-		return int16(binary.LittleEndian.Uint16(data)), nil
-	case UNSIGNED32:
-		return binary.LittleEndian.Uint32(data), nil
-	case INTEGER32:
-		return int32(binary.LittleEndian.Uint32(data)), nil
-	case UNSIGNED64:
-		return binary.LittleEndian.Uint64(data), nil
-	case INTEGER64:
-		return int64(binary.LittleEndian.Uint64(data)), nil
-	case REAL32:
-		parsed := binary.LittleEndian.Uint32(data)
-		return math.Float32frombits(parsed), nil
-	case REAL64:
-		parsed := binary.LittleEndian.Uint64(data)
-		return math.Float64frombits(parsed), nil
-	case VISIBLE_STRING, OCTET_STRING:
-		return string(data), nil
-	case DOMAIN:
-		return int64(0), nil
-	default:
-		return nil, ErrTypeMismatch
-	}
+	parsed := binary.LittleEndian.Uint32(v.value)
+	return math.Float32frombits(parsed), nil
 }
 
-// Decode byte array given the CANopen data type
-// Function will return either string, int64, uint64, or float64
-func DecodeToString(data []byte, dataType uint8, base int) (v string, e error) {
-	e = CheckSize(len(data), dataType)
-	if e != nil {
-		return "", e
+func (v *Variable) Float64() (float64, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	if v.DataType != REAL64 {
+		return 0, ErrTypeMismatch
 	}
-	// Cast to correct type
-	switch dataType {
-	case BOOLEAN, UNSIGNED8:
-		return strconv.FormatUint(uint64(data[0]), base), nil
-	case INTEGER8:
-		return strconv.FormatInt(int64(data[0]), base), nil
-	case UNSIGNED16:
-		return strconv.FormatUint(uint64(binary.LittleEndian.Uint16(data)), base), nil
-	case INTEGER16:
-		return strconv.FormatInt(int64(int16(binary.LittleEndian.Uint16(data))), base), nil
-	case UNSIGNED32:
-		return strconv.FormatUint(uint64(binary.LittleEndian.Uint32(data)), base), nil
-	case INTEGER32:
-		return strconv.FormatInt(int64(int32(binary.LittleEndian.Uint32(data))), base), nil
-	case UNSIGNED64:
-		return strconv.FormatUint(uint64(binary.LittleEndian.Uint64(data)), base), nil
-	case INTEGER64:
-		return strconv.FormatInt(int64(binary.LittleEndian.Uint64(data)), base), nil
-	case REAL32:
-		parsed := binary.LittleEndian.Uint32(data)
-		return strconv.FormatFloat(float64(math.Float32frombits(parsed)), 'f', -1, 64), nil
-	case REAL64:
-		parsed := binary.LittleEndian.Uint64(data)
-		return strconv.FormatFloat(math.Float64frombits(parsed), 'f', -1, 64), nil
+	parsed := binary.LittleEndian.Uint64(v.value)
+	return math.Float64frombits(parsed), nil
+}
+
+// Return value as string (only for STRING types)
+func (v *Variable) String() (string, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	switch v.DataType {
 	case VISIBLE_STRING, OCTET_STRING:
-		return string(data), nil
-	case DOMAIN:
-		return "0", nil
+		return string(v.value), nil
 	default:
 		return "", ErrTypeMismatch
 	}
 }
 
-// Decode the attribute in function of the of attribute type and pdo mapping for EDS entry
-func EncodeAttribute(accessType string, pdoMapping bool, dataType uint8) uint8 {
+// Return value as bool (only for BOOLEAN type)
+func (v *Variable) Bool() (bool, error) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
 
-	var attribute uint8
-
-	switch accessType {
-	case "rw":
-		attribute = AttributeSdoRw
-	case "ro", "const":
-		attribute = AttributeSdoR
-	case "wo":
-		attribute = AttributeSdoW
-	default:
-		attribute = AttributeSdoRw
+	if v.DataType != BOOLEAN {
+		return false, ErrTypeMismatch
 	}
-	if pdoMapping {
-		attribute |= AttributeTrpdo
+	if v.value[0] == 0 {
+		return false, nil
 	}
-	if dataType == VISIBLE_STRING || dataType == OCTET_STRING {
-		attribute |= AttributeStr
-	}
-	return attribute
+	return true, nil
 }
 
-// Encode attribute
-func DecodeAttribute(attribute uint8) string {
-	switch {
-	case attribute&AttributeSdoRw > 0:
-		return "rw"
-	case attribute&AttributeSdoR > 0:
-		return "ro"
-	case attribute&AttributeSdoW > 0:
-		return "wo"
-	default:
-		return "rw"
-	}
+// Return value as byte slice
+func (v *Variable) Bytes() []byte {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+
+	copied := make([]byte, len(v.value))
+	copy(copied, v.value)
+	return copied
 }
