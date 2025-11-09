529530b53fb03f9bd53f2f08d65d9c6fca7ae4a9
Samuel Lee <samfire.pilote@gmail.com>
2025-11-09 18:29:31 +0100

    lint: return error in file reader, check for errors in test for OD
    Also in AddSYNC and addPDO, don't check error but assign it to empty _

--- pkg/od/parser_v2.go
+++ pkg/od/parser_v2.go
@@ -43,7 +43,7 @@ func ParseV2(file any, nodeId uint8) (*ObjectDictionary, error) {
 		if err != nil {
 			return nil, err
 		}
-		defer f.Close()
+		defer func() { _ = f.Close() }()
 		bu = &bytes.Buffer{}
 		io.Copy(bu, f)
 
