d24a7b09c07a5fa8891b7927801e4c3d5c721332
Samuel Lee <54152208+samsamfire@users.noreply.github.com>
2026-08-13 12:04:34 +0200

    fix: a producer time of 0 should still start node and send boot-up (#69)

--- pkg/network/nmt_test.go
+++ pkg/network/nmt_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/samsamfire/gocanopen/v2/pkg/heartbeat"
 	"github.com/samsamfire/gocanopen/v2/pkg/nmt"
 	"github.com/samsamfire/gocanopen/v2/pkg/od"
 	"github.com/samsamfire/gocanopen/v2/pkg/sdo"
@@ -113,6 +114,43 @@ func TestNmt(t *testing.T) {
 		assert.Nil(t, err)
 		time.Sleep(1 * time.Second)
 		assert.Equal(t, nmt.StateStopped, local.NMT.GetInternalState())
+	})
+
+	t.Run("boot up with heartbeat producer disabled", func(t *testing.T) {
+		// A null producer heartbeat time disables the heartbeat producer, it
+		// should not prevent the node from booting
+		odict := od.Default()
+		err := odict.Index(0x1017).PutUint16(0, 0, true)
+		assert.Nil(t, err)
+
+		hbId := uint32(heartbeat.ServiceId) + uint32(NodeIdTest)
+		collector := &FrameCollector{}
+		_, err = net2.Subscribe(hbId, 0x7FF, false, collector)
+		assert.Nil(t, err)
+
+		local, err := net1.CreateLocalNode(NodeIdTest, odict)
+		assert.Nil(t, err)
+		defer net1.RemoveNode(NodeIdTest)
+
+		// The node should boot, and the boot-up message is mandatory even when
+		// the heartbeat producer is disabled
+		assert.Eventually(t, func() bool {
+			return local.NMT.GetInternalState() == nmt.StateOperational
+		}, 1*time.Second, 20*time.Millisecond)
+		frames := collector.GetFrames(hbId)
+		assert.Len(t, frames, 1)
+		if len(frames) > 0 {
+			assert.Equal(t, uint8(1), frames[0].DLC)
+			assert.Equal(t, nmt.StateInitializing, frames[0].Data[0])
+		}
+
+		// SDO should be served, i.e. the state change was propagated
+		_, err = net2.Configurator(NodeIdTest).ReadManufacturerSoftwareVersion()
+		assert.Nil(t, err)
+
+		// No cyclic heartbeat should follow, the producer is disabled
+		time.Sleep(500 * time.Millisecond)
+		assert.Len(t, collector.GetFrames(hbId), 1)
 	})
 
 	t.Run("boot up on reset", func(t *testing.T) {
