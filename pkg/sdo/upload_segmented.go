a874b7f377e43a9dbab1a59575f6fa21e45b83f0
Samuel Lee <samfire.pilote@gmail.com>
2026-09-17 21:07:09 +0200

    fix: rework readObjectDictionary
    Current implementation could break if intermediate buffer was too
    small when retransmiting during block transfer.

--- pkg/sdo/upload_segmented.go
+++ pkg/sdo/upload_segmented.go
@@ -44,7 +44,7 @@ func (s *SDOServer) txUploadSegment() error {
 	unread := s.buf.Len()
 
 	// Refill buffer if needed
-	err := s.readObjectDictionary(BlockSeqSize, -1, false)
+	err := s.readObjectDictionary(BlockSeqSize, false)
 	if err != nil {
 		return err
 	}
